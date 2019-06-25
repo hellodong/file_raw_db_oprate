92b6477772497c00f91e4e1ca59252a0a21b73a9
purefarmer <dong@debian.com>
2019-06-25 19:10:54 +0800

    test(): log 添加 err 宏定义打印

--- logx.h
+++ logx.h
@@ -6,6 +6,7 @@
 
 
 #define LOG(fmt,arg...)		do{printf("%s() #%d "fmt, __FUNCTION__,__LINE__,##arg);}while(0)
+#define LOG_ERR(fmt,arg...)		do{printf("%s() #%d [Error] "fmt, __FUNCTION__,__LINE__,##arg);}while(0)
 
 
 #endif
